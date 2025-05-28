bae79405def644817c002b5a22a51b49594d707f
Wouter van den Broeke <vandenbroekew@gmail.com>
2025-05-28 13:34:16 +0000

    Return success from onCreateCommand even if find failed

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,4 +4,6 @@ echo "export GRAALVM_HOME=$(sdk home java 24-graal)" >> ~/.bashrc
 sdk install java labsjdk-ce /data/jdk
 sdk default java labsjdk-ce
 mx -p /workspace/graal/substratevm intellijinit
-find /workspace/graal -name mxbuild -type d -print -exec rm -rf {} \;
+find /workspace/graal -name mxbuild -print -type d -exec rm -rf {} \;
+
+exit 0
