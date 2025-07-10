e252092b0f88730eec775f4744185a31b517868d
Wouter van den Broeke <vandenbroekew@gmail.com>
2025-07-10 14:59:53 +0000

    Move startup & shutdown hook registration under flag

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/profile/VirtualInvokeProfileFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/profile/VirtualInvokeProfileFeature.java
@@ -16,42 +16,37 @@ import java.lang.reflect.Method;
 public class VirtualInvokeProfileFeature implements InternalFeature  {
 
     @Override
-    public void beforeAnalysis(BeforeAnalysisAccess access) {
-        RuntimeSupport.getRuntimeSupport().addStartupHook(isFirstIsolate -> VirtualInvokeProfiler.enableProfiling());
-        RuntimeSupport.getRuntimeSupport().addShutdownHook(isFirstIsolate -> VirtualInvokeProfiler.dumpProfileData());
-        RuntimeSupport.getRuntimeSupport().addShutdownHook(isFirstIsolate -> {
-            try {
-                Class<?> clasz = Class.forName("org.h2.result.SearchRow");
-                System.out.println("Methods of SearchRow:");
-                for (var m : clasz.getFields()) {
-                    System.out.println(m);
-                }
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException("Failed to register class for reflection: " + e.getMessage(), e);
-            }
-        });
-    }
-
-    @Override
     public void duringSetup(DuringSetupAccess access) {
         try {
             Method m = VirtualInvokeProfiler.class.getDeclaredMethod("profileVirtualInvoke", String.class, Object.class, int.class);
             Method m2 = VirtualInvokeProfiler.class.getDeclaredMethod("enableProfiling");
-            Method m3 = InlineCacheTest.class.getDeclaredMethod("inlineCache", Object.class);
             Method foo = VirtualInvokeProfiler.class.getDeclaredMethod("foo");
             Method bar = VirtualInvokeProfiler.class.getDeclaredMethod("bar");
-            RuntimeReflection.register(m, m2, m3, foo, bar);
+            RuntimeReflection.register(m, m2, foo, bar);
         } catch (NoSuchMethodException ex) {
             throw new RuntimeException("Failed to register method for reflection: " + ex.getMessage(), ex);
         }
         RuntimeReflection.register(VirtualInvokeProfiler.class);
-        RuntimeReflection.register(InlineCacheTest.class);
     }
 
     @Override
     public void registerGraalPhases(Providers providers, Suites suites, boolean hosted) {
         if (!Boolean.getBoolean("disableVirtualInvokeProfilingPhase")) {
             suites.getHighTier().prependPhase(new InjectProfilingIntoVirtualCallsPhase());
+
+            RuntimeSupport.getRuntimeSupport().addStartupHook(isFirstIsolate -> VirtualInvokeProfiler.enableProfiling());
+            RuntimeSupport.getRuntimeSupport().addShutdownHook(isFirstIsolate -> VirtualInvokeProfiler.dumpProfileData());
+            RuntimeSupport.getRuntimeSupport().addShutdownHook(isFirstIsolate -> {
+                try {
+                    Class<?> clasz = Class.forName("org.h2.result.SearchRow");
+                    System.out.println("Methods of SearchRow:");
+                    for (var m : clasz.getFields()) {
+                        System.out.println(m);
+                    }
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException("Failed to register class for reflection: " + e.getMessage(), e);
+                }
+            });
         }
     }
 
